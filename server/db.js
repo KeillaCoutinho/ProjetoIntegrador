fd1c527c770a42b81001592b4eab8e83b89a0089
KeillaCoutinho <keillamaria02@gmail.com>
2026-09-13 14:32:13 -0300

    configura conexão segura com PostgreSQL

--- server/db.js
+++ server/db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'projeto_final',
-    password: '12345',
-    port: 5434
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
 });
 
 module.exports = pool;
